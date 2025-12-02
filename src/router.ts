fa6b819cb8fa3a26523228070d310860ce0a536f
Ulises Andrade <ulises.ag03@gmail.com>
2025-12-02 00:28:31 -0600

    api documentation for get, post and put

--- src/router.ts
+++ src/router.ts
@@ -8,7 +8,7 @@ const router = Router();
  * @swagger
  * components:
  *      schemas:
- *          Products:
+ *          Product:
  *              type: object
  *              properties: 
  *                  id:
@@ -29,8 +29,53 @@ const router = Router();
  *                      example: true
  */
 
+/**
+ * @swagger
+ * /api/products:
+ *      get:
+ *          summary: Get a list of products
+ *          tags:
+ *              - Products
+ *          description: Return a list of products
+ *          responses:
+ *              200:
+ *                  description: Successful response
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: array
+ *                              items:
+ *                                  $ref: '#/components/schemas/Product'
+ */
 router.get('/', getProducts);
 
+/**
+ * @swagger
+ * /api/products/{id}:
+ *  get:
+ *      summary: Get a Product by ID
+ *      tags:
+ *          - Products
+ *      description: Return a product based on its unique ID
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          description: The ID of the product to retrieve
+ *          required: true
+ *          schema:
+ *              type: integer
+ *      responses:
+ *          200:
+ *              description: Successful Response
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Product'
+ *          404:
+ *              description: Not found
+ *          400:
+ *              description: Bad Request - Invalid ID
+ */
 router.get('/:id', 
     param('id')
         .isInt().withMessage('ID no valido')
@@ -38,6 +83,37 @@ router.get('/:id',
     handleInputErrors,
     getProductById);
 
+/**
+ * @swagger
+ * /api/products:
+ *  post:
+ *      summary: Creates a new product
+ *      tags:
+ *          - Products
+ *      description: Returns a new record in the database
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          name:
+ *                              type: string
+ *                              example: "Monitor Curvo 49 pulgadas"
+ *                          price:
+ *                              type: number
+ *                              example: 399
+ *      responses:
+  *          201:
+ *              description: Successful response
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Product'
+ *          400:
+ *              description: Bad Request - Invalid input data
+ */
 router.post('/', 
     body('name')
         .notEmpty().withMessage('El nombre del producto no puede ir vacio'),
@@ -48,6 +124,49 @@ router.post('/',
     handleInputErrors,
     createProduct);
 
+/**
+ * @swagger
+ * /api/products/{id}:
+ *  put:
+ *      summary: Updates a product with user input
+ *      tags:
+ *          - Products
+ *      description: Returns the updated product
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          description: The ID of the product to retrieve
+ *          required: true
+ *          schema:
+ *              type: integer
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          name:
+ *                              type: string
+ *                              example: "Monitor Curvo 49 pulgadas"
+ *                          price:
+ *                              type: number
+ *                              example: 399
+ *                          availability:
+ *                              type: boolean
+ *                              example: true
+ *      responses:
+ *          200:
+ *              description: Successful response
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Product'
+ *          400:
+ *              description: Bad Request - Invalid ID or Invalid input data
+ *          404:
+ *              description: Product Not Found
+ */
 router.put('/:id', 
     body('name')
         .notEmpty().withMessage('El nombre del producto no puede ir vacio'),
